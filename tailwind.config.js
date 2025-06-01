5dff456bae2cd99b61c431824afc6d9dce4453d0
phaleixo <phaleixo@outlook.com.br>
2025-06-01 10:07:27 -0300

    modified:   index.html 	modified:   scripts/index.js 	new file:   styles/icons/debian-brands-solid.svg 	renamed:    image/banner/banner_debian12_emerald.png -> styles/images/banner/banner_debian12_emerald.png 	new file:   tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                debian: '#A80030',
+                debianEmerald: '#05475c',
+                debianLight: '#d63368',
+                debianDark: '#7a0022',
+            }
+        }
+    }
+};
